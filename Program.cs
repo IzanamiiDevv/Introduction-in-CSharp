f721d6a089f17cc8306dc91eeddbe78cc8411355
izanami <120745634+IzanamiiDevv@users.noreply.github.com>
2024-02-12 20:53:32 +0800

    add Files

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace IntroductionInCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Setup();
+            //Introduction
+            Console.WriteLine("Beep Boop Beep!");
+            Speak(3);
+            Console.WriteLine("Hello Im Orbit A digital assistant that created to assist you in this Program");
+            Console.WriteLine("But First! may i Know Your Name?");
+            Console.ReadLine();
+            Speak(3);
+            Console.WriteLine("Great Name! Thankyou For Giving your name");
+
+            //Close Program
+            Console.ReadKey();
+        }
+
+        static void Setup()
+        {
+            Console.Title = "Rock Paper Scissor Game!";
+        }
+
+        static void Speak(int t){
+            for (int i = 0; i < t; i++)
+            {
+                Console.Beep();
+            }
+        }
+    }
+}
